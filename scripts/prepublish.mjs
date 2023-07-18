4da64056ab931d3658a5e7b0118f137d95016446
Yaro <852373851@qq.com>
2023-07-18 14:56:05 +0800

    chore: release 1.0.10

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -1,19 +1,34 @@
 #!/usr/bin/env zx
-import 'zx/globals'
+import 'zx/globals';
+import { readFile, writeFile } from 'fs/promises';
 
-let { version } = JSON.parse(await fs.readFile('./package.json'))
+async function release() {
+  const packageFile = './package.json';
+  let packageData = await readFile(packageFile);
+  let { version } = JSON.parse(packageData);
 
-await $`pnpm install`
-await $`pnpm build`
+  // Increment the version number
+  const versionParts = version.split('.');
+  versionParts[2] = String(Number(versionParts[2]) + 1);
+  version = versionParts.join('.');
 
-await $`git add -A .`
-try {
-  await $`git commit -m 'chore: release ${version}' --allow-empty`
-  await $`git tag -m "v${version}" v${version}`
-  await $`git push --follow-tags`
-} catch (e) {
-  if (!e.stdout.includes('nothing to commit')) {
-    throw e
+  // Update the package.json file with the new version
+  const updatedPackageData = JSON.stringify({ ...JSON.parse(packageData), version }, null, 2);
+  await writeFile(packageFile, updatedPackageData);
+
+  await $`pnpm install && pnpm build`;
+
+  await $`git add -A .`;
+
+  try {
+    await $`git commit -m 'chore: release ${version}' --allow-empty`;
+    await $`git tag -m "v${version}" v${version}`;
+    await $`git push --follow-tags`;
+  } catch (e) {
+    if (!e.stdout.includes('nothing to commit')) {
+      throw e;
+    }
   }
-  await $`git tag -d v${version}`
 }
+
+await release();
